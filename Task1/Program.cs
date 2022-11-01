b3eb1b71075f39f825650262525a244a409338ee
Ovella <oks.vella@yandex.ru>
2022-11-01 19:49:48 +0300

    added task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+Console.Write("Enter a number: ");
+string num = Console.ReadLine();
+
+if (num.Length == 5)
+{
+    checkingNumber(num);
+}
+else 
+{
+    Console.WriteLine($"Enter correct number");
+}
+
+
+// -----------Methods-----------
+
+void checkingNumber(string num)
+{
+    if (num[0] == num[4] || num[1] == num[3])
+    {
+        Console.WriteLine($"Your number: {num} - palindrome.");
+    }
+    else
+    {
+        Console.WriteLine($"Your number: {num} - not palindrome.");
+    }
+}
