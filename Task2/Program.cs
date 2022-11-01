31cf7cc680b57f48b651d0e9ed478a43ec0f9d9b
Ovella <oks.vella@yandex.ru>
2022-11-02 01:23:34 +0300

    added task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int randomValue = new Random().Next(2, 10);
+Console.WriteLine("Random value = " + randomValue);
+
+for (int i = 1; i <= randomValue; i++)
+{
+    Console.Write(i * i * i);
+    if (i < randomValue)
+    {
+        Console.Write(", ");
+    }
+}
+
+
+
+
